b682a0bc1425c54e24e90168232c50d2ecea364c
GeoIoan98 <geoioan14@gmail.com>
2021-03-20 23:46:35 +0000

    Finished store

--- Front End/server.py
+++ Front End/server.py
@@ -11,6 +11,15 @@ app = Flask(__name__)
 def hello():
     return render_template('restaurant.html') # can include name = "name" as well, not sure where it can be used tho
 
+@app.route('/store')
+def store():
+    return render_template('store.html') # can include name = "name" as well, not sure where it can be used tho
+
+@app.route('/leaderboard')
+def leader():
+    return render_template('leaderboard.html') # can include name = "name" as well, not sure where it can be used tho
+
+
 @app.route('/')
 @app.route('/intro')
 def intro():
